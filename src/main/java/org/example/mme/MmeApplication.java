502089ad8e3233d86ad71ade5f7c399e08960372
Estheuz <Luumcletaja@gmail.com>
2025-06-29 19:41:43 -0300

    🎉 add project base🎉

---
+++ src/main/java/org/example/mme/MmeApplication.java
@@ -0,0 +1,13 @@
+package org.example.mme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmeApplication.class, args);
+    }
+
+}
